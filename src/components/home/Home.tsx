a962994247693075f6d657c8d452bfffd001474b
Abror Saparbaev <asaparbaev89@gmail.com>
2020-09-20 19:23:39 +0500

    useStyle.js

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,16 @@
 import React from "react"
 import {gql} from "@apollo/client"
 import {Typography} from "@material-ui/core"
+import {useStyle} from './useStyle'
+
 
 export const Home = ({data}) => {
     const {name} = data.viewer;
+
+    const classes = useStyle();
+
     return (
-        <div>
+        <div className={classes.root}>
             <Typography>{name}</Typography>
         </div>
     )
