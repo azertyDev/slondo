2e6ad1cf14642ffa58d1db7d5f37b54fb9dbd8fe
Abror Saparbaev <asaparbaev89@gmail.com>
2020-10-02 15:48:01 +0500

    Category slider

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import {MainSlider} from '../header/sliders/mainSlider/MainSlider'
-import CategorySlider from '../header/sliders/categorySlider/CategorySlider'
+import {CategorySlider} from '../header/sliders/categorySlider/CategorySlider'
 import {MainContent} from './mainContent/MainContent'
 import {Footer} from '../footer/Footer'
+import {Container} from "@material-ui/core";
 
 export const Home = () => {
     return (
         <>
             <MainSlider/>
-            <CategorySlider/>
-            <MainContent/>
+            <Container maxWidth='lg'>
+                <CategorySlider/>
+                <MainContent/>
+            </Container>
             <Footer/>
         </>
     )
