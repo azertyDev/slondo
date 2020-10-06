8b3484114fb372df584ff93bbdb87893201aa38b
azertyDev <khikmatovumid@mail.ru>
2020-10-06 15:14:00 +0500

    Refactoring preview page

--- src/components/advertisement/previewAdvertisement/PreviewAdvertisement.tsx
+++ src/components/advertisement/previewAdvertisement/PreviewAdvertisement.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container, Grid, Typography } from '@material-ui/core'
+import { AdvertisementSelect } from '../advertisementSelect/AdvertisementSelect'
 
 // styles
 import { useStyles } from './useStyles'
@@ -18,6 +19,56 @@ export const PreviewAdvertisement = () => {
                         >
                             Проверка объявления
                         </Typography>
+                    </Grid>
+
+                    <Grid
+                        container
+                        item
+                        xs={12}
+                        direction="row"
+                        justify="space-between"
+                        spacing={2}
+                    >
+                        <Grid
+                            item
+                            xs={12}
+                            sm={6}
+                            md={3}
+                            lg={3}
+                            xl={3}
+                            container
+                            alignItems="center"
+                        >
+                            <Typography variant="subtitle1">
+                                Тип объявления:
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6} md={3} lg={3}>
+                            <Typography variant="subtitle1">
+                                Объявление
+                            </Typography>
+                        </Grid>
+
+                        <Grid
+                            item
+                            xs={12}
+                            sm={6}
+                            md={3}
+                            lg={3}
+                            xl={3}
+                            container
+                            alignItems="center"
+                        >
+                            <Typography variant="subtitle1">
+                                Категория:
+                            </Typography>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6} md={3} lg={3}>
+                            <Typography variant="subtitle1">
+                                Тип объявления:
+                            </Typography>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container>
