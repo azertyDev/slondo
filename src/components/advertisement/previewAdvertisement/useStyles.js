90881701d404074afa1fb5c58ddba016fa9d4970
azertyDev <khikmatovumid@mail.ru>
2020-10-06 15:34:27 +0500

    test

--- src/components/advertisement/previewAdvertisement/useStyles.js
+++ src/components/advertisement/previewAdvertisement/useStyles.js
@@ -1,6 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 export const useStyles = makeStyles((theme) => ({
-    root: {},
+    root: {
+        textOverflow: 'ellipsis',
+    },
     title: { margin: '50px 0' },
 }))
