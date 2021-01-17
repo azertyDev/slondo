0847548496b233c9f673ae9edfa7078c7c782881
Abror Saparbaev <asaparbaev89@gmail.com>
2021-01-17 20:25:58 +0500

    refactor for build

--- src/components/cabinet/useStyles.js
+++ src/components/cabinet/useStyles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles(() => ({
     root: {
         '& h6.menu-title': {
             fontWeight: '600',
