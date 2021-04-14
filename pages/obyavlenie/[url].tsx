69f8bf7dd14f32191df7e69d770ef9c87a472c36
azertyDev <khikmatovumid@mail.ru>
2021-04-14 18:01:01 +0500

    Fix CardItem.tsx

---
+++ pages/obyavlenie/[url].tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react';
+import {ShowPostContainer} from '@src/components/post/show_post/ShowPostContainer';
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {GetStaticPaths, GetStaticProps} from "next";
+
+const ShowPost: FC = () => <ShowPostContainer/>;
+
+export const getStaticProps: GetStaticProps = async ({locale}) => ({
+    props: {
+        ...await serverSideTranslations(
+            locale,
+            ['post', 'categories', 'common', 'cabinet', 'header', 'footer', 'auth_reg']
+        )
+    }
+});
+
+export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
+    return {
+        paths: [], //indicates that no page needs be created at build time
+        fallback: 'blocking' //indicates the type of fallback
+    }
+};
+
+export default ShowPost;
---
+++ src/components/elements/card/card_item/CardItem.tsx
@@ -0,0 +1,174 @@
+import React, {FC, useState} from 'react';
+import Link from 'next/link';
+import {useTranslation} from 'react-i18next';
+import {Card, CardActionArea, CardContent, CardMedia, IconButton, Tooltip, Typography} from '@material-ui/core';
+import Skeleton from '@material-ui/lab/Skeleton';
+import {DeliveryIcon, FavoriteIcon, SafeIcon, SwapIcon} from '@src/components/elements/icons';
+import {InnerCardData} from '@root/interfaces/CardData';
+import {numberPrettier, transformToCyrillic} from '@src/helpers';
+import {userAPI} from '@src/api/api';
+import {useSelector} from 'react-redux';
+import {RootState} from '@src/redux/rootReducer';
+import {useStyles} from './useStyles';
+
+
+type CardItemProps = {
+    isFetch: boolean;
+} & InnerCardData;
+
+export const CardItem: FC<CardItemProps> = (props) => {
+    const {
+        id,
+        isFetch,
+        image,
+        delivery,
+        safe_deal,
+        exchange,
+        title,
+        price,
+        created_at,
+        currency,
+        ads_type,
+        region,
+        city
+    } = props;
+
+    const isFavorite = true;
+
+    const {t} = useTranslation(['common']);
+    const translatedTitle = transformToCyrillic(title);
+
+    const {isAuth} = useSelector((store: RootState) => store.user);
+
+    const [liked, setLiked] = useState(false);
+
+    const date = new Date(created_at);
+
+    const months = [
+        'января',
+        'февраля',
+        'марта',
+        'апреля',
+        'мая',
+        'июня',
+        'июля',
+        'августа',
+        'сентября',
+        'октября',
+        'ноября',
+        'декабря'
+    ];
+
+    const formatted_date = `${date.getDate()} ${months[date.getMonth()]} ${date.getHours() + ':' + date.getMinutes()}`;
+
+    const handleFavorite = () => {
+        setLiked(!liked);
+        userAPI.favoriteAds(id);
+    };
+
+    const classes = useStyles({ads_type, isFavorite});
+    return (
+        <div className={classes.root}>
+            {isAuth && (
+                <IconButton
+                    className="favorite-btn" onClick={handleFavorite}
+                >
+                    <FavoriteIcon />
+                </IconButton>
+            )}
+            <Link href={`/obyavlenie/${translatedTitle}-${id}`}>
+                <a target='_blank'>
+                    <Card elevation={0} title={title}>
+                        {isFetch ? (
+                            <Skeleton
+                                variant="rect"
+                                className={classes.skeleton}
+                            />
+                        ) : (
+                            <CardMedia
+                                className="card-media"
+                                image={image ?? '/img/card-logo.png'}
+                            >
+                                <div className="card-header">
+                                    <div className="title">
+                                        <Typography variant="subtitle2">
+                                            {t(ads_type)}
+                                        </Typography>
+                                    </div>
+                                    <div className="icons">
+                                        {!!delivery && (
+                                            <Tooltip
+                                                title="Есть доставка"
+                                                arrow
+                                            >
+                                                <span>
+                                                    <DeliveryIcon/>
+                                                </span>
+                                            </Tooltip>
+                                        )}
+                                        {!!safe_deal && (
+                                            <Tooltip
+                                                title="Безопасная покупка"
+                                                arrow
+                                            >
+                                                <span>
+                                                    <SafeIcon/>
+                                                </span>
+                                            </Tooltip>
+                                        )}
+                                        {!!exchange && (
+                                            <Tooltip
+                                                title="Возможен обмен"
+                                                arrow
+                                            >
+                                                <span>
+                                                    <SwapIcon/>
+                                                </span>
+                                            </Tooltip>
+                                        )}
+                                    </div>
+                                </div>
+                            </CardMedia>
+                        )}
+                        <CardActionArea>
+                            <CardContent>
+                                {isFetch
+                                    ? <>
+                                        <Skeleton variant="rect"/>
+                                        <Skeleton variant="rect"/>
+                                        <Skeleton variant="rect"/>
+                                        <br/>
+                                        <Skeleton variant="rect"/>
+                                    </>
+                                    : <>
+                                        <Typography
+                                            variant="subtitle1"
+                                            color="initial"
+                                            noWrap
+                                        >
+                                            {title}
+                                        </Typography>
+                                        <Typography
+                                            variant="h5"
+                                            color="initial"
+                                        >
+                                            {numberPrettier(price)}
+                                            <span> {t(currency.name)}</span>
+                                        </Typography>
+                                        <Typography variant="caption" noWrap>
+                                            {`${region.name}, ${city.name}`}
+                                        </Typography>
+                                        <Typography variant="caption">
+                                            {formatted_date}
+                                        </Typography>
+                                    </>}
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                </a>
+            </Link>
+        </div>
+    )
+}
+
+
