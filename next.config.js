9a7706a7691c7666c7bbdf18d626d7f07b410465
Abror Saparbaev <asaparbaev89@gmail.com>
2021-07-02 09:44:58 +0500

    migrate to preact build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const withPreact = require('next-plugin-preact');
 const {i18n} = require('./next-i18next.config');
 
-module.exports = {
+module.exports = withPreact({
     i18n
-};
+});
