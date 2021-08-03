d38c85add1b708e50d386a7513741bca1c84e860
Abror <ASaparbaev89@gmail.com>
2021-08-03 10:46:27 +0500

    add google tag manager to body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,17 +37,13 @@ export default class MyDocument extends Document {
                     />
                 </Head>
                 <body>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MPMDTGC" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+                    }}>
+                </noscript>
                 <Main/>
                 <NextScript/>
-                <noscript>
-                    <iframe
-                        height="0"
-                        width="0"
-                        style={{display: 'none', visibility: 'hidden'}}
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-MPMDTGC"
-                    >
-                    </iframe>
-                </noscript>
                 </body>
             </Html>
         );
