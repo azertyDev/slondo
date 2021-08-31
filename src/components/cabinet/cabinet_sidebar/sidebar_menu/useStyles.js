35c0273d3e20bb138f38eb389a679718c5e7839e
Sepr26 <rasulsaparbaev@gmail.com>
2021-08-31 13:25:20 +0500

    adaptive cabinet sidebar

--- src/components/cabinet/cabinet_sidebar/sidebar_menu/useStyles.js
+++ src/components/cabinet/cabinet_sidebar/sidebar_menu/useStyles.js
@@ -2,13 +2,23 @@ import {makeStyles} from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
     root: {
+        '& > div.layout-container': {
+            padding: 0
+        },
         '& .menu-item': {
             display: 'flex',
             flexDirection: 'column',
             backgroundColor: '#F2F2F2',
-            padding: 15,
-            borderRadius: 10,
-            marginBottom: 10,
+            padding: '15px',
+            borderRadius: '10px',
+            marginBottom: '10px',
+            [theme.breakpoints.down('xs')]: {
+                padding: 0,
+                paddingLeft: '14px',
+                borderRadius: 0,
+                marginBottom: 0,
+                margin: '8px 0'
+            },
             '&:last-child': {
                 marginBottom: 0
             },
@@ -20,6 +30,12 @@ export const useStyles = makeStyles((theme) => ({
                 background: '#fff',
                 justifyContent: 'center',
                 boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.25)',
+                [theme.breakpoints.down('xs')]: {
+                    background: '#F2F2F2',
+                    boxShadow: 'none',
+                    borderRadius: 0,
+                    justifyContent: 'flex-start'
+                },
                 '& .MuiTypography-body1': {
                     fontSize: '1rem',
                     [theme.breakpoints.down('md')]: {
@@ -27,19 +43,28 @@ export const useStyles = makeStyles((theme) => ({
                     },
                     [theme.breakpoints.down('sm')]: {
                         fontSize: 'calc(12px + (100vw - 576px) / 1280)'
+                    },
+                    [theme.breakpoints.down('xs')]: {
+                        fontSize: '1rem'
                     }
                 },
                 '& .MuiListItemText-root': {
                     flex: 'none'
                 },
                 '& svg': {
-                    marginRight: 10,
-                    [theme.breakpoints.between('sm','md')]: {
+                    marginRight: '10px',
+                    [theme.breakpoints.down('xs')]: {
+                        marginRight: '27px',
+                        '& path': {
+                            fill: '#838383'
+                        }
+                    },
+                    [theme.breakpoints.between('sm', 'md')]: {
                         display: 'none'
                     }
                 }
             }
-        },
+        }
     },
     selected: {
         border: '1px solid #AD66D5',
