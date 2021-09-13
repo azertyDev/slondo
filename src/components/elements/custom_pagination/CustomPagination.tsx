75ef91bd50ffce8ee350d63728aa4b4a3d5d3c49
azertyDev <khikmatovumid@mail.ru>
2021-09-13 11:00:12 +0500

    Modify styling CustomPagination

--- src/components/elements/custom_pagination/CustomPagination.tsx
+++ src/components/elements/custom_pagination/CustomPagination.tsx
@@ -1,5 +1,7 @@
 import {FC} from 'react';
 import Pagination from '@material-ui/lab/Pagination';
+import {useStyles} from './useStyles';
+import {useMediaQuery, useTheme} from '@material-ui/core';
 
 export type CustomPaginationTypes = {
     totalItems: number,
@@ -16,15 +18,21 @@ export const CustomPagination: FC<CustomPaginationTypes> = (props) => {
         handlePagePagination
     } = props;
 
+    const isXsDown = useMediaQuery(useTheme().breakpoints.down('xs'));
+    const classes = useStyles();
     const count = Math.ceil((totalItems !== 0 ? totalItems : 1) / itemsPerPage);
 
     return (
         <Pagination
+            classes={{
+                root: classes.root
+            }}
             count={count}
             page={currentPage}
             onChange={handlePagePagination}
+            size={isXsDown ? 'small' : 'medium'}
             shape="rounded"
-            variant="outlined"
+            color="secondary"
         />
     );
 };
