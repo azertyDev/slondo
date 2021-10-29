b9608c9d6a63004caf3c12ef096eabe87dfdccea
azertyDev <khikmatovumid@mail.ru>
2021-10-29 15:21:08 +0500

    Modify CommonFormPreview

--- src/components/post/create_post/third_step/common_form/CommonFormPreview.tsx
+++ src/components/post/create_post/third_step/common_form/CommonFormPreview.tsx
@@ -254,11 +254,11 @@ export const CommonFormPreview: FC<DefaultParamsPropsType> = (props) => {
                     </Typography>
                 </Grid>
                 <Grid item xs={12}>
-                    <Typography variant="subtitle1">
-                        <pre className={classes.description}>
-                            {values.description.trim()}
-                        </pre>
-                    </Typography>
+                    <pre className={classes.description}>
+                        <Typography variant="subtitle1">
+                                {values.description.trim()}
+                        </Typography>
+                    </pre>
                 </Grid>
             </Grid>
         </>
