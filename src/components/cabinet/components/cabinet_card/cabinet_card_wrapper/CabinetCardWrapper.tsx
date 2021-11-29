fc472f29fdd8951ffb490a211f3a49b9c585d8be
Codellac <asaparbaev89@gmail.com>
2021-11-29 14:18:16 +0500

    disable promote

--- src/components/cabinet/components/cabinet_card/cabinet_card_wrapper/CabinetCardWrapper.tsx
+++ src/components/cabinet/components/cabinet_card/cabinet_card_wrapper/CabinetCardWrapper.tsx
@@ -212,6 +212,7 @@ export const CabinetCardWrapper: FC<CabinetCardPropsType> = props => {
                                     <>
                                         {isPublic && (
                                             <CustomButton
+                                                disabled
                                                 className="icons"
                                                 onClick={handlePromoteOpen}
                                             >
