4b4edf5ef94974cedefbc84d908814e47816c79d
Codellac <asaparbaev89@gmail.com>
2022-11-09 14:41:50 +0500

    fix for 1 cpu count run

--- server.mjs
+++ server.mjs
@@ -12,7 +12,7 @@ const port = 3317;
 const serverUrl = 'http://localhost';
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
-const numCPUs = cpus().length;
+const cpuCount = cpus().length;
 const handle = app.getRequestHandler();
 
 console.log('dev mode: ' + dev);
@@ -27,37 +27,48 @@ if (dev) {
         });
     });
 } else {
-    if (cluster.isPrimary) {
-        console.log(`Primary ${process.pid} is running`);
+    if (cpuCount > 1) {
+        if (cluster.isPrimary) {
+            console.log(`Primary ${process.pid} is running`);
 
-        // Fork workers.
-        for (let i = 0; i < numCPUs - 1; i++) {
-            cluster.fork();
+            // Fork workers.
+            for (let i = 0; i < cpuCount - 1; i++) {
+                cluster.fork();
+            }
+
+            cluster.on('exit', (worker, code, signal) => {
+                if (signal) {
+                    console.log(`worker was killed by signal: ${signal}`);
+                } else if (code !== 0) {
+                    console.log(`worker exited with error code: ${code}`);
+                } else {
+                    console.log('worker success!');
+                }
+                console.log(`worker ${worker.process.pid} died`);
+            });
         }
 
-        cluster.on('exit', (worker, code, signal) => {
-            if (signal) {
-                console.log(`worker was killed by signal: ${signal}`);
-            } else if (code !== 0) {
-                console.log(`worker exited with error code: ${code}`);
-            } else {
-                console.log('worker success!');
-            }
-            console.log(`worker ${worker.process.pid} died`);
-        });
-    }
-
-    if (cluster.isWorker) {
+        if (cluster.isWorker) {
+            app.prepare().then(() => {
+                createServer((req, res) => {
+                    const parsedUrl = parse(req.url, true);
+                    handle(req, res, parsedUrl);
+                }).listen(port, (err) => {
+                    if (err) throw err;
+                    console.log(`Server started on ${serverUrl}:${port}`);
+                });
+            });
+            console.log(`Worker ${process.pid} started`);
+        }
+    } else {
         app.prepare().then(() => {
             createServer((req, res) => {
-                const parsedUrl = parse(req.url, true);
-                handle(req, res, parsedUrl);
+                handle(req, res, req.url);
             }).listen(port, (err) => {
                 if (err) throw err;
                 console.log(`Server started on ${serverUrl}:${port}`);
             });
         });
-        console.log(`Worker ${process.pid} started`);
     }
 }
 
